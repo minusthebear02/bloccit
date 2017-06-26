4593adf226f90e3de74468cd7c4ea3dd78f6506c
Mark Froehlich <mfroeh2@gmail.com>
2017-06-25 23:00:42 -0400

    Assignment 23: Complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase if email.present? }
+  before_save {
+    name_array = name.split
+    name_array.each do |n|
+      n.capitalize!
+    end
+    self.name = name_array.join(" ")
+    }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -11,4 +18,5 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,12 @@ RSpec.describe User, type: :model do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+
+    it "should format user's name" do
+      user.name = "mark froehlich"
+      user.save
+      expect(user.name).to eq "Mark Froehlich"
+    end
   end
 
   describe "invalid user" do
@@ -36,4 +42,5 @@ RSpec.describe User, type: :model do
       expect(user_with_invalid_email).to_not be_valid
     end
   end
+
 end
