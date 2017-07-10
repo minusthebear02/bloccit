1375739890531eb5164d8c9fe1cd248c7167db8f
Mark Froehlich <mfroeh2@gmail.com>
2017-07-10 08:26:08 -0400

    Assignment 32: Complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def written_posts_yet?
+    current_user && current_user.posts.count != 0
+  end
+
+  def written_comments_yet?
+    current_user && current_user.comments.count != 0
+  end
+
+  def favorited_posts_yet?
+    current_user && current_user.favorites.count != 0
+  end
 end
